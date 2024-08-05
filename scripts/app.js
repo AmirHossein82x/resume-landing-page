af9d3db9d724dbcea7255135f53555eab3aeffe6
amir hossein <amirhosseing983@gmail.com>
2024-08-05 14:20:35 +0330

    make scroll

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ const menu = document.querySelector(".menu")
 const cover = document.querySelector(".cover")
 const resumeListItems = document.querySelectorAll(".resume-list__item")
 const portfolioListItems = document.querySelectorAll(".portfolio-list__item")
+const menuItems = document.querySelectorAll(".menu__item")
+const sections = document.querySelectorAll("main > section")
 
 
 navToggleIcon.addEventListener("click", function() {
@@ -35,3 +37,41 @@ portfolioListItems.forEach((item) => {
         document.querySelector(portfolioId).classList.add("portfolio-content--active")
     })
 })
+
+
+const observer = new IntersectionObserver(observerHandler, {threshold: 0.5});
+
+function observerHandler(allSections) {
+    allSections.map((section) => {
+        let sectionClassName = section.target.className
+        if (section.isIntersecting) {
+            console.log(sectionClassName)
+            document.querySelector(`.menu__item[data-section=${sectionClassName}]`).classList.add("menu__item--active")
+        }else {
+            
+            document.querySelector(`.menu__item[data-section=${sectionClassName}]`).classList.remove("menu__item--active")
+        }
+    })
+    
+}
+
+
+sections.forEach((section) => {
+    observer.observe(section)
+})
+menuItems.forEach((item) => {
+    item.addEventListener("click", function(e){
+        e.preventDefault()
+        document.querySelector(".menu__item--active").classList.remove("menu__item--active")
+        item.classList.add("menu__item--active")
+        let sectionClass = item.getAttribute("data-section")
+        window.scrollTo({
+            top: document.getElementById(sectionClass).offsetTop - 200,
+            behavior: "smooth"
+        })
+        
+        
+
+    })  
+})
+
